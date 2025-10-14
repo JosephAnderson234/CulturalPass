b22e074883f1bf3f4f1c28f16b740fb09018da3e
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-10-14 01:05:44 -0500

    feat: Implementar componentes de esqueleto para las páginas de Login y Registro

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,16 @@
-"use client";
-import LoginForm from '@src/components/auth/LoginForm';
+"use client"
+import dynamic from 'next/dynamic'
+import LoginSkeleton from '@src/components/skeletons/Auth'
 
-
-
+const LoginForm = dynamic(() => import('@src/components/auth/LoginForm'), {
+    ssr: false,
+    loading: () => <LoginSkeleton />,
+})
 
 export default function LoginPage() {
     return (
-        <div className='w-full flex flex-col items-center justify-center min-h-screen'>
-            
-            <div className='w-full max-w-md p-8 space-y-3 rounded-xl bg-background-secondary'>
+        <div className="w-full flex flex-col items-center justify-center min-h-screen">
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-background-secondary">
                 <LoginForm />
             </div>
         </div>
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,13 +1,16 @@
-"use client";
+"use client"
+import dynamic from 'next/dynamic'
+import { RegisterSkeleton } from '@src/components/skeletons/Auth'
 
-import RegisterForm from "@src/components/auth/RegisterForm";
-
-
+const RegisterForm = dynamic(() => import('@src/components/auth/RegisterForm'), {
+    ssr: false,
+    loading: () => <RegisterSkeleton />,
+})
 
 export default function RegisterPage() {
     return (
-        <div className='w-full flex flex-col items-center justify-center min-h-screen'>
-            <div className='w-full max-w-4xl p-8 space-y-3 rounded-xl bg-background-secondary'>
+        <div className="w-full flex flex-col items-center justify-center min-h-screen">
+            <div className="w-full max-w-4xl p-8 space-y-3 rounded-xl bg-background-secondary">
                 <RegisterForm />
             </div>
         </div>
